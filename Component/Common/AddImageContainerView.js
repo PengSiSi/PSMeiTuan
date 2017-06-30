1f02fe8679b913fa691a26f056cf7486873e49ae
PengSiSi <15300086553@163.com>
2017-06-30 09:40:57 +0800

    实现多选图片并展示图片

---
+++ Component/Common/AddImageContainerView.js
@@ -0,0 +1,115 @@
+/**
+ * Created by 思思 on 17/5/7.
+ * 
+ * // 可以声明 prop 为指定的 JS 基本类型。默认
+    // 情况下，这些 prop 都是可传可不传的。
+    optionalArray: React.PropTypes.array,
+    optionalBool: React.PropTypes.bool,
+    optionalFunc: React.PropTypes.func,
+    optionalNumber: React.PropTypes.number,
+    optionalObject: React.PropTypes.object,
+    optionalString: React.PropTypes.string,
+
+    // 所有可以被渲染的对象：数字，
+    // 字符串，DOM 元素或包含这些类型的数组。
+    optionalNode: React.PropTypes.node,
+
+    // React 元素
+    optionalElement: React.PropTypes.element,
+
+    // 用 JS 的 instanceof 操作符声明 prop 为类的实例。
+    optionalMessage: React.PropTypes.instanceOf(Message),
+
+    // 用 enum 来限制 prop 只接受指定的值。
+    optionalEnum: React.PropTypes.oneOf(['News', 'Photos']),
+
+    // 指定的多个对象类型中的一个
+    optionalUnion: React.PropTypes.oneOfType([
+      React.PropTypes.string,
+      React.PropTypes.number,
+      React.PropTypes.instanceOf(Message)
+    ]),
+
+    // 指定类型组成的数组
+    optionalArrayOf: React.PropTypes.arrayOf(React.PropTypes.number),
+
+    // 指定类型的属性构成的对象
+    optionalObjectOf: React.PropTypes.objectOf(React.PropTypes.number),
+
+    // 特定形状参数的对象
+    optionalObjectWithShape: React.PropTypes.shape({
+      color: React.PropTypes.string,
+      fontSize: React.PropTypes.number
+    }),
+
+    // 以后任意类型加上 `isRequired` 来使 prop 不可空。
+    requiredFunc: React.PropTypes.func.isRequired,
+
+    // 不可空的任意类型
+    requiredAny: React.PropTypes.any.isRequired,
+
+    // 自定义验证器。如果验证失败需要返回一个 Error 对象。不要直接
+    // 使用 `console.warn` 或抛异常，因为这样 `oneOfType` 会失效。
+    customProp: function(props, propName, componentName) {
+      if (!/matchme/.test(props[propName])) {
+        return new Error('Validation failed!');
+      }
+    }
+  },
+ */
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity
+} from 'react-native';
+
+
+export default class extends Component {
+
+     // 定义相关属性类型
+    static PropTypes = {
+        imageArr: View.propTypes.array,
+    };
+
+    render() {
+        let views = [];
+        const {imageArr} = this.props;
+        for (var index in imageArr) {
+            var element = imageArr[index];
+            views.push(
+                <View>
+                    <Image source>
+                        <Image>
+                        </Image>
+                    </Image>
+                </View>
+            );
+            }
+        }
+        return (
+        );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+    imageItemStyle: {
+        width: 50,
+        height: 50,
+        backgroundColor: 'white'
+    },
+    deleteButtonStyle: {
+        width: 20,
+        height: 20,
+        marginRight: 0,
+        marginTop: 0
+    }
+});
--- Component/Sections/Setting/Demos/AddImagesDemo.js
+++ Component/Sections/Setting/Demos/AddImagesDemo.js
@@ -1,5 +1,6 @@
 /**
  * Created by 思思 on 17/6/28.
+ * 该库的使用参考博客: http://www.jianshu.com/p/8420b08062c7
  */
 import React, { Component } from 'react';
 import {
@@ -10,6 +11,9 @@ import {
 } from 'react-native';
 
 import Color from './../../../Config/Color';
+import ImagePicker from 'react-native-image-crop-picker';
+import AddImageContainerView from './../../../Common/AddImageContainerView';
+import Space from './../../../Config/Space';
 
 export default class extends Component {
 
@@ -21,17 +25,34 @@ export default class extends Component {
         headerStyle: {
             backgroundColor: Color.kMainColor  // 设置导航栏的背景颜色,headerTintColor设置无效
         },
-    }); 
+    });
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            files: [],
+        }
+    } 
 
     render() {
+        const { files } = this.state;
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    发现
-                </Text>
+                <Text onPress={()=>{this.pickerImagesAction()}}>选择图片</Text>
+                <AddImageContainerView style={styles.ImageContainerViewStyle}></AddImageContainerView>
             </View>
+
         );
     }
+
+    // 选择多张图片
+    pickerImagesAction() {
+        ImagePicker.openPicker({
+            multiple: true
+            }).then(images => {
+            console.log(images);
+            });
+        }
 }
 
 const styles = StyleSheet.create({
@@ -41,4 +62,10 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: 'white',
     },
+    ImageContainerViewStyle: {
+        width: Space.kScreenWidth - 20,
+        margin: 20,
+        height: 200,
+        backgroundColor: 'gray'
+    }
 });
